f6883373f532b8b110c29069b591139475a0966c
Ezenwa Onukwue <eonukwue@gmail.com>
2020-09-21 04:34:19 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,9 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  low_value = nil
+  low_key = nil
   name_hash.collect do |key, value|
-    low_value = nil
-    low_key = nil
+
     if low_value <= nil
       key
     end
